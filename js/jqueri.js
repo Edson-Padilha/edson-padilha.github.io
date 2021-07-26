3b1a3d8b37768684d578c29a4fb4781af2f4ed95
Edson-Padilha <60894182+Edson-Padilha@users.noreply.github.com>
2021-07-26 17:14:08 -0300

    Add files via upload
    expandindo todos os accordions.

--- js/jqueri.js
+++ js/jqueri.js
@@ -1,3 +1,17 @@
 function inicio(){
     $("html, body").animate({scrollTop: 0}, 300);
 }
+function expandir() {    
+    
+    $("#collapseTwo").collapse('show');
+    $("#collapseThree").collapse('show');
+    $("#collapseFor").collapse('show');
+    $("#collapseFive").collapse('show');
+    $("#collapseSix").collapse('show');
+    $("#collapseSeven").collapse('show');
+    $("#collapseEight").collapse('show');
+    $("#collapseNine").collapse('show');
+    $("#collapseTen").collapse('show');
+    $("#collapseOne").collapse('show');
+}
+
